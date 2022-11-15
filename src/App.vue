429d2d6086b46b910c6a6da672483f6717b16950
freddycarta <cartafederico92@gmail.com>
2022-11-15 17:37:53 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ message }}</h1>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return{
+      message: 'La mia prima app con vite',
+    }
+  }
+}
+ 
+</script>
+
+<style>
+h1 {
+  color: blue;
+}
+
+
+  
+</style>
